365032f5f48ad518c33591f7950158a2b24a542d
kaiquemoreira <kaique.moreira@truckpad.com.br>
2024-10-31 18:16:02 -0300

    feat: generate first migration

---
+++ alembic/versions/a2a2e2f19fc7_create_bank_accounts_table.py
@@ -0,0 +1,41 @@
+"""create bank accounts table
+
+Revision ID: a2a2e2f19fc7
+Revises: 
+Create Date: 2024-10-31 17:03:07.795510
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'a2a2e2f19fc7'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # Create the sequence
+    op.execute(sa.text("CREATE SEQUENCE account_number_seq START 1"))
+
+    # Create the table
+    op.create_table('bank_accounts',
+        sa.Column('id', sa.Uuid(), nullable=False),
+        sa.Column('account_number', sa.Integer(), nullable=False, server_default=sa.text("nextval('account_number_seq')")),
+        sa.Column('balance', sa.Integer(), nullable=False),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('account_number'),
+        sa.UniqueConstraint('id')
+    )
+
+
+def downgrade() -> None:
+    # Drop the table
+    op.drop_table('bank_accounts')
+
+    # Drop the sequence
+    op.execute(sa.text("DROP SEQUENCE account_number_seq"))
