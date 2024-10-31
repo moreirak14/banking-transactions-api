7d1fdf474e5dbc6285c76eb34e961ac3206735d4
kaiquemoreira <kaique.moreira@truckpad.com.br>
2024-10-31 18:16:28 -0300

    feat: config dynaconf

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+from dynaconf import Dynaconf
+
+root_path = os.path.dirname(os.path.realpath(__file__))
+
+settings = Dynaconf(
+    root_path=root_path,
+    envvar_prefix="DYNACONF",
+    settings_files=['settings.toml', '.secrets.toml'],
+    environments=["default"],
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
